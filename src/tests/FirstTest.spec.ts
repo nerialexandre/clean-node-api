72fc5096eae3bb4c3edf5196e6bb2f5af631be4b
Alexandre Neri <carlosalexandre.neri@gmail.com>
2021-12-27 09:09:27 -0300

    fix: fix linter

--- src/tests/FirstTest.spec.ts
+++ src/tests/FirstTest.spec.ts
@@ -1,9 +1,8 @@
-import { User } from "../models/User"
+import { User } from '../models/User'
 
 test('it should be ok', () => {
-  const user = new User
+  const user = new User()
 
   user.name = 'Diego'
   expect(user.name).toEqual('Diego')
 })
-
